9ee96f053613a9dea3437c93c494962746cf7c12
Maksim Gavrilov <gavrilovmaksim91@gmail.com>
2026-04-07 22:29:11 +0300

    Исправлен расчет скорости

--- main/main.c
+++ main/main.c
@@ -130,10 +130,12 @@ static void can_on_recv(const can_frame_t *frame)
     switch (frame->id)
     {
     case CAN_ID_ASC1:
-        uint16_t speed_raw = ((uint16_t)frame->data[1] << 8 | frame->data[2]) >> 4;
-        float speed_kmh = speed_raw / 8.0f;
+        uint16_t speed_combined = (uint16_t)frame->data[2] << 8 | frame->data[1];
+        uint16_t speed_raw = (speed_combined >> 4) & 0x0FFF;
+        // float speed_kmh = speed_raw / 8.0f;
         esp_lv_adapter_lock(-1);
-        lv_subject_set_int(&subjects.speed, (int)speed_kmh);
+        // lv_subject_set_int(&subjects.speed, (int)speed_kmh);
+        lv_subject_set_int(&subjects.speed, speed_raw);
         esp_lv_adapter_unlock();
         break;
     case CAN_ID_SWS1:
